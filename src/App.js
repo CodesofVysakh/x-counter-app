bf01e3ba9b5307e362bf5a0250b4ba1bb2952f2c
CodesOfVysakh <onvysakh309@gmail.com>
2024-07-13 22:01:15 +0530

    XCounter app initialised with functionality

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css"
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [count, setCount] = useState(0);
+
+    return (
+        <main>
+            <h1>Counter App</h1>
+            <p>Count: {count}</p>
+            <button
+                onClick={() => {
+                    setCount((prev) => prev + 1);
+                }}
+            >
+                Increment
+            </button>
+            <button
+                onClick={() => {
+                    setCount((prev) => prev - 1);
+                }}
+            >
+                Decrement
+            </button>
+        </main>
+    );
 }
 
 export default App;
